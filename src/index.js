8f09ad783217a127464fd31e26fc123c8cb7edfa
Cypher14x <alexcoro14x@gmail.com>
2025-02-14 10:10:17 -0500

    Visualizando el Callback Hell

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import {buscarHeroe} from './js/callbacks'
 import './styles.css';
 
 const heroeId = 'capi';
+const heroeId2 = 'iron';
 
 buscarHeroe(heroeId, (err,heroe)=>{
-    if (err) {
-        console.error(err);
-    }else{
-        console.log('Callback llamado');
-        console.log(heroe);
-    }
+    if (err) {return console.error(err);}
+    // }else{
+    //     console.log('Callback llamado');
+    //     console.log(heroe);
+    // }
+    buscarHeroe(heroeId2, (err, heroe2)=>{
+        if (err) {return console.error(err);}
+        console.log(`Enviando a ${heroe.nombre} y ${heroe2.nombre} a la mision`);
+        
+    });
 });
 
